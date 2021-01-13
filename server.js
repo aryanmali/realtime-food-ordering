c6fdbfa6dce9685449eb5e331123e06ad1e58c7c
aryanrvmali <aryanmali@users.noreply.github.com>
2021-01-13 13:09:49 +0530

    Laravel-mix configured

--- server.js
+++ server.js
@@ -6,6 +6,11 @@ const path = require('path')
 
 const PORT = process.env.PORT || 3300;
 
+//Assets
+app.use(express.static('public'))
+
+
+//Routes
 app.get('/', (req,res) => {
     res.render('home');
 } )
