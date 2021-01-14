c767e52f3e780906ba1de9f4e2ce8596c1209c0d
aryanrvmali <aryanmali@users.noreply.github.com>
2021-01-14 19:26:51 +0530

    sessions(cookies) managed

--- server.js
+++ server.js
@@ -1,9 +1,13 @@
+require('dotenv').config()
 const express = require('express')
 const app = express()
 const ejs = require('ejs')
 const expressLayout = require('express-ejs-layouts')
 const path = require('path')
 const mongoose = require('mongoose')
+const session = require('express-session')
+const flash = require('express-flash')
+const MongoDbStore = require('connect-mongo')(session)
 const PORT = process.env.PORT || 3300;
 
 //Database Connection
@@ -15,6 +19,22 @@ connection.once('open', () => {
 }).catch(err => {
     console.log('Connection failed...')
 });
+
+//Session Store
+let mongoStore = new MongoDbStore({
+                mongooseConnection: connection,
+                collection: 'sessions'
+            }) 
+
+//Session Configuration
+app.use(session({
+    secret: process.env.COOKIE_SECRET,
+    resave: false,
+    store: mongoStore,
+    saveUninitialized: false,
+    cookie: { maxAge: 1000*60*60*24} // 24 Hours
+}))
+app.use(flash())
 
 //Assets
 app.use(express.static('public'))
