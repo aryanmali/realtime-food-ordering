6fb0af48e8eecbd9613ee758e9155ad3fd001d1b
aryanrvmali <aryanmali@users.noreply.github.com>
2021-01-16 11:55:53 +0530

    orders page bugs fixed

--- app/http/controllers/authController.js
+++ app/http/controllers/authController.js
@@ -3,6 +3,11 @@ const bcrypt = require('bcrypt')
 const passport = require('passport')
 
 function authController() {
+
+    const _getRedirectedUrl = (req) => {
+        return req.user.role === 'admin' ? 'admin/orders' : 'customer/orders'
+    }
+
     return {
         login(req, res) {
             res.render('auth/login')
@@ -28,7 +33,7 @@ function authController() {
                         req.flash('error', info.message)
                         return next(err)
                     }
-                    return res.redirect('/')
+                    return res.redirect(_getRedirectedUrl(req))
                 })
             })(req, res, next)
         },
