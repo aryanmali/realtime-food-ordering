e5173b38c8d69aa05f4fd7d49fca32d27f9a3591
aryanrvmali <aryanmali@users.noreply.github.com>
2021-01-16 20:53:05 +0530

    order status updates dynamically

--- routes/web.js
+++ routes/web.js
@@ -2,7 +2,8 @@ const homeController = require('../app/http/controllers/homeController');
 const authController = require('../app/http/controllers/authController');
 const cartController = require('../app/http/controllers/customers/cartController');
 const orderController = require('../app/http/controllers/customers/orderController');
-const AdminOrderController = require('../app/http/controllers/admin/orderController');
+const adminOrderController = require('../app/http/controllers/admin/orderController');
+const statusController = require('../app/http/controllers/admin/statusController');
 
 //Middlewares
 const guest = require('./../app/http/middleware/guest')
@@ -29,7 +30,8 @@ function initRoutes(app){
     app.get('/customer/orders', auth, orderController().index)
 
     //admin
-    app.get('/admin/orders', admin, AdminOrderController().index)
+    app.get('/admin/orders', admin, adminOrderController().index)
+    app.post('/admin/order/status', admin, statusController().update)
 
 }
 
