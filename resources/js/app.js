d28caf42e30c89882cdf41c17d3c39840e1d7575
aryanrvmali <aryanmali@users.noreply.github.com>
2021-01-23 09:49:45 +0530

    socket.io integration

--- resources/js/app.js
+++ resources/js/app.js
@@ -40,8 +40,6 @@ if(alertMsg) {
     }, 2000)
 }
 
-initAdmin()
-
 // Change order status
 let statuses = document.querySelectorAll('.status_line')
 let hiddenInput = document.querySelector('#hiddenInput')
@@ -73,3 +71,30 @@ function updateStatus(order) {
 }
 
 updateStatus(order);
+
+//Socket
+ let socket = io()
+
+ //Join
+ if(order) {
+    socket.emit('join', `order_${order._id}`)
+}
+
+let adminAreaPath = window.location.pathname
+if(adminAreaPath.includes('admin')) {
+    initAdmin(socket)
+    socket.emit('join', 'adminRoom')
+}
+
+socket.on('orderUpdated', (data) => {
+    const updatedOrder = { ...order }
+    updatedOrder.updatedAt = moment().format()
+    updatedOrder.status = data.status
+    updateStatus(updatedOrder)
+    new Noty({
+        type: 'success',
+        timeout: 1000,
+        text: 'Order updated',
+        progressBar: false,
+    }).show();
+})
